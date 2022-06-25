1af3b39259f77ffa3f15860c80262e49468ecd35
Spiridonov_Sergey <letuchline@mail.ru>
2022-06-25 16:22:04 +0300

    add Example006Max

---
+++ Example006Max/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 2;
+int b = 4;
+int c = 8;
+int d = 3;
+int e = 1;
+int max = a;
+
+if(max < b) max = b;
+if(max < c) max = c;
+if(max < d) max = d;
+if(max < e) max = e;
+
+Console.Write("Максимальное = ");
+Console.Write(max);
