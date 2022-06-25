c695235420a86607b1043805d1f2d7166d7aa682
Spiridonov_Sergey <letuchline@mail.ru>
2022-06-25 16:07:31 +0300

    add Example005HiMasha

---
+++ Example005HiMasha/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+String username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Приветствую вас госпожа Мария");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
