636d8ccbce7977921bcd1f314132574c47c6cf20
Spiridonov_Sergey <letuchline@mail.ru>
2022-06-25 15:21:06 +0300

    add Example003_A+B

---
+++ Example003_A+B/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введи число А");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число В");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Сумма равна: ");
+Console.Write(number1 + number2);
