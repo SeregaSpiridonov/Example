60e2bc0435df7be2f853b048be3f922e85897247
Spiridonov_Sergey <letuchline@mail.ru>
2022-06-28 12:02:08 +0300

    add Example011Array

---
+++ Example011Array/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection) //Заполняет массив случайными числами от 1 до 10
+{
+    int length=collection.Length;
+    int index = 0;
+    while(index<length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+void PrintArray(int[] col)  //Сдесь задаётся вывод массива на экран
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.Write(col[position]+", ");
+        position++;
+    }
+}
+Console.WriteLine("Введите размер массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];  //new int заменяет то что в функциях новым int?
+
+FillArray(array);
+PrintArray(array);
