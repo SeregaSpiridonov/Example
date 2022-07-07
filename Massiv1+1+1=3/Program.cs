75488dbd13ef0cae703bfcf2d923e1cb95558296
Spiridonov_Sergey <letuchline@mail.ru>
2022-07-07 14:51:53 +0300

    add Massiv1 and Seminar4Switch

---
+++ Massiv1+1+1=3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+string numString = num.ToString();
+int[] arrayNumbers = new int[numString.Length];
+
+for(int i=0; i < numString.Length; i++)
+{
+    arrayNumbers[i] = int.Parse(numString[i]+ "");
+}
+for(int j=0; j<arrayNumbers.Length; j++)
+{
+    sum = sum + arrayNumbers[j];
+}
+Console.WriteLine($"Сумма цифр в числе равна {sum}.");
---
+++ Seminar4Switch/Program.cs
@@ -0,0 +1,29 @@
+﻿// See Напишите программу, которая по заданному номеру четверти, показывает 
+//диапазон возможных координат точек в этой четверти (x и y).
+
+
+int num = Convert.ToInt32(Console.ReadLine()); //пример со свитч
+string FindCord(int number)
+{    
+    switch(number) 
+    {
+        case 1:
+        {
+            return "x,y от 1 до 9999";
+        }
+        case 2:
+        {
+            return "x от -1 до -9999 ,y от 1 до 9999";
+        }
+        case 3:
+        {
+            return "x,y от -1 до -9999";
+        }
+        case 4:
+        {
+            return "x от 1 до 9999, y от -1 до -9999";
+        }
+    }
+    return "Error";
+}
+Console.WriteLine(FindCord(num));
